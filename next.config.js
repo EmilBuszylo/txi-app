b2bb6840d3758875afe7cef74aad0c5efa79c491
Emil Buszyło <emil.buszylo@olx.pl>
2023-08-17 18:45:11 +0200

    ci: change cors config

--- next.config.js
+++ next.config.js
@@ -16,10 +16,8 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://www.txi-zlecenia.pl',
-          },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://txi-app.vercel.app' },
         ],
       },
     ];
