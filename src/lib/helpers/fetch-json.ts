bc4a17993751fb97dbd2e788d6a97f441a50a5b6
Emil Buszyło <emil.buszylo@olx.pl>
2023-09-20 18:38:14 +0200

    chore: add logs

--- src/lib/helpers/fetch-json.ts
+++ src/lib/helpers/fetch-json.ts
@@ -37,6 +37,7 @@ export default async function fetchJson<JSON = unknown>(
   init?: RequestInit
 ): Promise<JSON> {
   try {
+    logger.error({ input, init, stack: 'fetchJson error' });
     const response = await fetch(input, {
       ...init,
       headers: {
@@ -44,11 +45,11 @@ export default async function fetchJson<JSON = unknown>(
         ...init?.headers,
       },
     });
-
+    logger.error(2);
     // if the server replies, there's always some data in json
     // if there's a network error, it will throw at the previous line
     const data = await response.json();
-
+    logger.error(3, { data, response });
     // response.ok is true when res.status is 2xx
     // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
     if (response.ok) {
