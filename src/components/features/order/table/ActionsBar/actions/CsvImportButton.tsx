f679a027534f9b458c01ed2a7238e554defe648c
Emil Buszyło <emil.buszylo@olx.pl>
2023-10-30 09:36:51 +0100

    fix: fix cv import button

--- src/components/features/order/table/ActionsBar/actions/CsvImportButton.tsx
+++ src/components/features/order/table/ActionsBar/actions/CsvImportButton.tsx
@@ -4,10 +4,12 @@ import { MouseEvent } from 'react';
 
 import { formatDate } from '@/lib/helpers/date';
 import { downloadFile } from '@/lib/helpers/downloadFile';
+import { logger } from '@/lib/logger';
 
 import { ActionButton } from '@/components/features/order/table/ActionsBar/ActionsBar';
 import { translateOrderItemKeys } from '@/components/features/order/table/ActionsBar/translateOrderItemKey';
 import { ActionsBarProps } from '@/components/ui/data-table/data-table';
+import { useToast } from '@/components/ui/use-toast';
 
 import { dateFormats } from '@/constant/date-formats';
 import { CollectionPoint } from '@/server/collection-points.ts/collectionPoint';
@@ -18,6 +20,7 @@ type CsvImportButtonProps = Pick<ActionsBarProps<Order[]>, 'selectedItems' | 'se
 
 export const CsvImportButton = ({ selectedAmount, selectedItems }: CsvImportButtonProps) => {
   const parser = new Parser();
+  const { toast } = useToast();
 
   return (
     <ActionButton
@@ -25,58 +28,64 @@ export const CsvImportButton = ({ selectedAmount, selectedItems }: CsvImportButt
       icon={FileSpreadsheet}
       isDisabled={!selectedAmount}
       onClick={(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-        const parsedItems = selectedItems.map((item) => {
-          const itemKeys = Object.keys(item);
-          const newItems: Record<string, unknown> = {};
+        try {
+          const parsedItems = parseOrdersData(selectedItems);
 
-          for (const itemKey of itemKeys) {
-            if (translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]) {
-              if (itemKey === 'locationFrom' || itemKey === 'locationTo') {
-                newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
-                  (item[itemKey as keyof Order] as unknown as LocationFrom)?.address?.fullAddress ||
-                  '';
-              } else if (
-                itemKey === 'locationVia' &&
-                typeof item[itemKey as keyof Order] === 'object'
-              ) {
-                let val = '';
-                for (const via of item[itemKey as keyof Order] as unknown as LocationVia[]) {
-                  const fullAddress = via?.address?.fullAddress || '';
-
-                  val = val === '' ? val + fullAddress : val + ' + ' + fullAddress;
-                }
-
-                newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
-                  val;
-              } else if (itemKey === 'collectionPoint') {
-                newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
-                  (item[itemKey as keyof Order] as unknown as CollectionPoint)?.fullAddress || '';
-              } else if (itemKey === 'driver') {
-                newItems[
-                  translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]
-                ] = `${(item[itemKey as keyof Order] as unknown as Driver).firstName} ${
-                  (item[itemKey as keyof Order] as unknown as Driver).lastName
-                }`;
-              } else {
-                newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
-                  parseCsvValue(item[itemKey as keyof Order], itemKey);
-              }
-            }
-          }
-
-          return newItems;
-        });
-
-        const csv = parser.parse(parsedItems);
-        e.preventDefault();
-        return downloadFile({
-          data: csv,
-          fileName: 'zlecenia.csv',
-          fileType: 'text/csv',
-        });
+          const csv = parser.parse(parsedItems);
+          e.preventDefault();
+          return downloadFile({
+            data: csv,
+            fileName: 'zlecenia.csv',
+            fileType: 'text/csv',
+          });
+        } catch (error) {
+          toast({
+            description:
+              'Wystąpił błąd i nie można wygenerować pliku CSV, prosimy spróbować później.',
+            variant: 'destructive',
+          });
+          logger.error(error);
+        }
       }}
     />
   );
+};
+
+const parseOrdersData = (selectedItems: CsvImportButtonProps['selectedItems']) => {
+  return selectedItems.map((item) => {
+    const itemKeys = Object.keys(item);
+    const newItems: Record<string, unknown> = {};
+
+    for (const itemKey of itemKeys) {
+      if (translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]) {
+        if (itemKey === 'locationFrom' || itemKey === 'locationTo') {
+          newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
+            (item[itemKey as keyof Order] as unknown as LocationFrom)?.address?.fullAddress || '';
+        } else if (itemKey === 'locationVia' && typeof item[itemKey as keyof Order] === 'object') {
+          let val = '';
+          for (const via of item[itemKey as keyof Order] as unknown as LocationVia[]) {
+            const fullAddress = via?.address?.fullAddress || '';
+
+            val = val === '' ? val + fullAddress : val + ' + ' + fullAddress;
+          }
+
+          newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] = val;
+        } else if (itemKey === 'collectionPoint') {
+          newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
+            (item[itemKey as keyof Order] as unknown as CollectionPoint)?.fullAddress || '';
+        } else if (itemKey === 'driver') {
+          newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] = `${
+            (item[itemKey as keyof Order] as unknown as Driver)?.firstName || ''
+          } ${(item[itemKey as keyof Order] as unknown as Driver)?.lastName || ''}`;
+        } else {
+          newItems[translateOrderItemKeys[itemKey as keyof typeof translateOrderItemKeys]] =
+            parseCsvValue(item[itemKey as keyof Order], itemKey);
+        }
+      }
+    }
+
+    return newItems;
+  });
 };
 
 const parseCsvValue = (value: Order[keyof Order], keyName?: string): Order[keyof Order] => {
